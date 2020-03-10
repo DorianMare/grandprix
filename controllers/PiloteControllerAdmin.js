2e0e9d6165a3273711c2e8a09efe31d4bb553492
Dorian <dorianmare@yahoo.fr>
2020-03-10 11:35:54 +0100

    refactor pays sans doublons et suppression upload

--- controllers/PiloteControllerAdmin.js
+++ controllers/PiloteControllerAdmin.js
@@ -1,8 +1,7 @@
 let model = require('../models/pilote');
 var async = require('async');
 let modelEcurie = require('../models/ecurie');
-
-
+let modelPays = require('../models/pays');
 
 // ///////////////////////// R E P E R T O I R E    D E S    P I L O T E S
 module.exports.ListeAllPilotes = function (request, response) {
@@ -17,56 +16,69 @@ module.exports.ListeAllPilotes = function (request, response) {
 };
 
 module.exports.FormulaireAjoutPilote = function (request, response) {
-   modelEcurie.getAllEcuries(function (err, result) {
-      if (err) {
-         console.log(err);
-         return;
+   async.parallel([
+      function (callback) {
+         modelEcurie.getAllEcuries(function (err, result)  {
+            callback(null, result);
+         })
+      },
+      function (callback) {
+         modelPays.getListePays(function (err, result) {
+            callback(null, result);
+         })
       }
-      response.FormulaireAjoutPilote = result;
-      response.render('ajouterPilote', response);
-   });
+   ],
+      function (err, result) {
+         if (err) {
+            console.log(err);
+            return;
+         }
+         response.listeEcurie = result[0];
+         response.pays = result[1];
+         response.render('ajouterPilote', response);
+      })
 };
 
-   module.exports.AjouterPilotePost = function (request, response) {
-    let data = request.body;
+module.exports.AjouterPilotePost = function (request, response) {
+   let data = request.body;
 
-    if (data["ECUNUM"] == 'NULL') {
-        delete data["ECUNUM"];
-    }
+   if (data["ECUNUM"] == 'NULL') {
+      delete data["ECUNUM"];
+   }
 
-    let file = request.files.PHOADRESSE;
+   let file = request.files.PHOADRESSE;
 
-    function pause (time) {
-        return new Promise((resolve) => setTimeout(resolve, time));
-    }
+   function pause(time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+   }
 
-    async.parallel ([
-            function (callback) {
-                model.ajouterPilote(data, function (err, result) {callback(null,result)});
-            },
-            function (callback) {
-                pause(100).then(() => {
-                    model.ajouterPhotoPilote(file.name,function (err, result) {callback(null,result)});
-                });
-            },
-        ],
-        function (err, result) {
+   async.parallel([
+      function (callback) {
+         model.ajouterPilote(data, function (err, result) { callback(null, result) });
+      },
+      function (callback) {
+         pause(100).then(() => {
+            model.ajouterPhotoPilote(file.name, function (err, result) { callback(null, result) });
+         });
+      },
+   ],
+      function (err, result) {
+         if (err) {
+            // gestion de l'erreur
+            console.log(err);
+            return;
+         }
+         file.mv("./public/image/pilote/" + file.name, function (err, result) {
             if (err) {
-                // gestion de l'erreur
-                console.log(err);
-                return;
+               console.log(err);
+            } else {
+               console.log('Upload');
             }
-            file.mv("./public/image/pilote/"+file.name, function (err,result){
-              if (err) {
-                console.log(err);
-              } else {
-                console.log('Upload');
-              }
-            });
+         });
 
-            response.ajouterPilote = result[0];
-            response.ajouterPhotoPilote = result[1];
-            response.render('ajouterPilotePost', response);
-        }
-    );
+         response.ajouterPilote = result[0];
+         response.ajouterPhotoPilote = result[1];
+         response.render('ajouterPilotePost', response);
+      }
+   );
 };
